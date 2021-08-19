7fb613f7a45fe06a989a2656f2e0762f975ab8bb
Pranav <pranav@Shadowfaxs-MacBook-Air.local>
2021-08-19 05:40:05 +0530

    added about-us, minor improvements in website, changed logo

--- coreengine/urls.py
+++ coreengine/urls.py
@@ -7,10 +7,11 @@ urlpatterns = [
 	path('home/', core_views.Home.as_view(), name='home'),
 	path('curriculum/', core_views.Curriculum.as_view(), name='curriculum'),
 	path('admission/', core_views.Admission.as_view(), name='admission'),
-	path('banner/<str:banner_name>', core_views.Banner.as_view(), name='banner'),
+	path('templates/banner/<str:banner_name>', core_views.Banner.as_view(), name='banner'),
 	path('faculties/', core_views.Faculties.as_view(), name='faculties'),
 	path('founders/', core_views.Founders.as_view(), name='faculties'),
 	path('principal/', core_views.Principal.as_view(), name='faculties'),
 	path('infrastructure/', core_views.Infrastructure.as_view(), name='faculties'),
 	path('contact/', core_views.Contact.as_view(), name='faculties'),
+	path('about-us/', core_views.AboutUs.as_view(), name='faculties'),
 ]
--- coreengine/views.py
+++ coreengine/views.py
@@ -14,13 +14,11 @@ from coreengine import models
 
 
 class Home(APIView):
-
 	def get(self, request):
 		# return HttpResponse("This is Home Page.")
 		return render(request, 'index.html')
 
 class Banner(APIView):
-
 	def get(self, request, banner_name):
 		image_data = open("./templates/banner/"+str(banner_name), "rb").read()
 		return HttpResponse(image_data, content_type="image/png")
@@ -29,47 +27,45 @@ class Banner(APIView):
 
 
 class Curriculum(APIView):
-
 	def get(self, request):
 		# return HttpResponse("This is Home Page.")
 		return render(request, 'curriculum.html')
 
 class Founders(APIView):
-
 	def get(self, request):
 		# return HttpResponse("This is Home Page.")
 		return render(request, 'founders.html')
 
 class Principal(APIView):
-
 	def get(self, request):
 		# return HttpResponse("This is Home Page.")
 		return render(request, 'principal.html')
 
 class Faculties(APIView):
-
 	def get(self, request):
 		# return HttpResponse("This is Home Page.")
 		return render(request, 'faculties.html')
 
 
 class Admission(APIView):
-
 	def get(self, request):
 		# return HttpResponse("This is Home Page.")
 		return render(request, 'admission.html')
 
 
 class Infrastructure(APIView):
-
 	def get(self, request):
 		# return HttpResponse("This is Home Page.")
 		return render(request, 'infrastructure.html')
 
 
 class Contact(APIView):
-
 	def get(self, request):
 		# return HttpResponse("This is Home Page.")
 		return render(request, 'contact.html')
+
+class AboutUs(APIView):
+	def get(self, request):
+		# return HttpResponse("This is Home Page.")
+		return render(request, 'about-us.html')
 
